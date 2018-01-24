6f3e14ccb05f9a866816e3f537f400df179b763a
MohanrajS19 <mohanraj.sambath@ibotsystems.com>
2018-01-24 17:28:06 +0530

    Implemented Sample Video recording without Zoom feature using surfaceview.

--- surfaceviewcamerarecording/src/main/java/com/surfaceviewcamerarecording/MainActivity.java
+++ surfaceviewcamerarecording/src/main/java/com/surfaceviewcamerarecording/MainActivity.java
@@ -1,13 +1,291 @@
 package com.surfaceviewcamerarecording;
 
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.pm.ActivityInfo;
+import android.media.MediaRecorder;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback,View.OnClickListener,GestureDetector.OnGestureListener{
+    //private final String VIDEO_PATH_NAME = "/mnt/sdcard/VGA_30fps_512vbrate.mp4";
+    private final String VIDEO_PATH_NAME = "/mnt/sdcard/SRFrec_1.mp4";
+    private MediaRecorder mMediaRecorder;
+    private Camera mCamera;
+    private SurfaceView mSurfaceView;
+    private SurfaceHolder mHolder;
+    private View mToggleButton;
+    private ImageView imgVw_recording;
+    private boolean mInitSuccesful = false;
+    public boolean isRecord = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // we shall take the video in landscape orientation
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        mSurfaceView = (SurfaceView) findViewById(R.id.surfaceView);
+        mHolder = mSurfaceView.getHolder();
+        mHolder.addCallback(this);
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        imgVw_recording = (ImageView) findViewById(R.id.imgVw_recording);
+        imgVw_recording.setOnClickListener(this);
+
+    }
+
+    /* Init the MediaRecorder, the order the methods are called is vital to
+     * its correct functioning */
+    private void initRecorder(Surface surface) throws IOException {
+        // It is very important to unlock the camera before doing setCamera
+        // or it will results in a black preview
+        if(mCamera == null) {
+            mCamera = android.hardware.Camera.open();
+            mCamera.unlock();
+        }
+
+        if(mMediaRecorder == null)  mMediaRecorder = new MediaRecorder();
+        mMediaRecorder.setPreviewDisplay(surface);
+        mMediaRecorder.setCamera(mCamera);
+
+        mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.DEFAULT);
+        //       mMediaRecorder.setOutputFormat(8);
+        mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+        mMediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
+        mMediaRecorder.setVideoEncodingBitRate(512 * 1000);
+        mMediaRecorder.setVideoFrameRate(30);
+        mMediaRecorder.setVideoSize(640, 480);
+        mMediaRecorder.setOutputFile(VIDEO_PATH_NAME);
+
+        try {
+            mMediaRecorder.prepare();
+        } catch (IllegalStateException e) {
+            // This is thrown if the previous calls are not called with the
+            // proper order
+            e.printStackTrace();
+        }
+
+        mInitSuccesful = true;
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        try {
+            if(!mInitSuccesful)
+                initRecorder(mHolder.getSurface());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        shutdown();
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
+    private void shutdown() {
+        // Release MediaRecorder and especially the Camera as it's a shared
+        // object that can be used by other applications
+        mMediaRecorder.reset();
+        mMediaRecorder.release();
+        mCamera.release();
+
+        // once the objects have been released they can't be reused
+        mMediaRecorder = null;
+        mCamera = null;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.imgVw_recording:
+                System.out.println("isRecord:"+isRecord);
+                if(!isRecord) {
+                    mMediaRecorder.start();
+                    imgVw_recording.setImageResource(!isRecord ? R.drawable.stop : R.drawable.record);
+                    isRecord=true;
+                }else{
+                    imgVw_recording.setImageResource(isRecord ? R.drawable.record : R.drawable.stop);
+                    mMediaRecorder.stop();
+                    mMediaRecorder.reset();
+                    isRecord=false;
+                }
+                break;
+
+        }
+
+    }
+
+    @Override
+    public boolean onDown(MotionEvent motionEvent) {
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float velocityX, float velocityY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        return false;
     }
 }
+
+
+/*=====================================================*/
+/*-----------------------------------------------------*/
+
+
+/*public class MediaRecorderRecipe extends Activity implements SurfaceHolder.Callback {
+    private final String VIDEO_PATH_NAME = "/mnt/sdcard/VGA_30fps_512vbrate.mp4";
+
+    private MediaRecorder mMediaRecorder;
+    private Camera mCamera;
+    private SurfaceView mSurfaceView;
+    private SurfaceHolder mHolder;
+    private View mToggleButton;
+    private boolean mInitSuccesful;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.media_recorder_recipe);
+
+        // we shall take the video in landscape orientation
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        mSurfaceView = (SurfaceView) findViewById(R.id.surfaceView);
+        mHolder = mSurfaceView.getHolder();
+        mHolder.addCallback(this);
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        mToggleButton = (ToggleButton) findViewById(R.id.toggleRecordingButton);
+        mToggleButton.setOnClickListener(new OnClickListener() {
+            @Override
+            // toggle video recording
+            public void onClick(View v) {
+                if (((ToggleButton)v).isChecked()) {
+                    mMediaRecorder.start();
+                    try {
+                        Thread.sleep(10 * 1000); // This will recode for 10 seconds, if you don't want then just remove it.
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    finish();
+                }
+                else {
+                    mMediaRecorder.stop();
+                    mMediaRecorder.reset();
+                    try {
+                        initRecorder(mHolder.getSurface());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+    *//* Init the MediaRecorder, the order the methods are called is vital to
+     * its correct functioning *//*
+    private void initRecorder(Surface surface) throws IOException {
+        // It is very important to unlock the camera before doing setCamera
+        // or it will results in a black preview
+        if(mCamera == null) {
+            mCamera = Camera.open();
+            mCamera.unlock();
+        }
+
+        if(mMediaRecorder == null)  mMediaRecorder = new MediaRecorder();
+        mMediaRecorder.setPreviewDisplay(surface);
+        mMediaRecorder.setCamera(mCamera);
+
+        mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.DEFAULT);
+        //       mMediaRecorder.setOutputFormat(8);
+        mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+        mMediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
+        mMediaRecorder.setVideoEncodingBitRate(512 * 1000);
+        mMediaRecorder.setVideoFrameRate(30);
+        mMediaRecorder.setVideoSize(640, 480);
+        mMediaRecorder.setOutputFile(VIDEO_PATH_NAME);
+
+        try {
+            mMediaRecorder.prepare();
+        } catch (IllegalStateException e) {
+            // This is thrown if the previous calls are not called with the
+            // proper order
+            e.printStackTrace();
+        }
+
+        mInitSuccesful = true;
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            if(!mInitSuccesful)
+                initRecorder(mHolder.getSurface());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        shutdown();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {}
+
+    private void shutdown() {
+        // Release MediaRecorder and especially the Camera as it's a shared
+        // object that can be used by other applications
+        mMediaRecorder.reset();
+        mMediaRecorder.release();
+        mCamera.release();
+
+        // once the objects have been released they can't be reused
+        mMediaRecorder = null;
+        mCamera = null;
+    }*/
+/*=====================================================*/
