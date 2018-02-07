e32e2a6101f98e49b6514a9b2a0c4eb66e09a076
MohanrajS19 <mohanraj.sambath@ibotsystems.com>
2018-02-07 13:11:18 +0530

    Text Clock Implemented.

--- mytime/src/main/java/com/mytime/activity/MainActivity.java
+++ mytime/src/main/java/com/mytime/activity/MainActivity.java
@@ -1,13 +1,18 @@
-package com.mytime;
+package com.mytime.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextClock;
 import android.widget.TextView;
 
+import com.mytime.R;
+
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
 private Button button_In_Time,button_Out_Time,button_punch;
@@ -25,7 +30,12 @@ private Button button_In_Time,button_Out_Time,button_punch;
         button_Out_Time =(Button)findViewById(R.id.button_Out_Time);
         button_punch =(Button)findViewById(R.id.button_punch);
         txtVw_val_status =(TextView)findViewById(R.id.txtVw_val_status);
+
         button_punch.setOnClickListener(this);
+        TextClock textClock = (TextClock) findViewById(R.id.textClock);
+        textClock.setFormat12Hour(null);
+        //textClock.setFormat24Hour("dd/MM/yyyy hh:mm:ss a");
+        textClock.setFormat24Hour("hh:mm:ss a  EEE MMM d");
     }
 
     @Override
@@ -33,11 +43,11 @@ private Button button_In_Time,button_Out_Time,button_punch;
         switch (v.getId()){
             case R.id.button_punch:
                 if(is_PunchIn){
-                getCurrentTime("IN");
+                getCurrentTime("Check-IN");
                 is_PunchIn=false;
                 }
                 else {
-                getCurrentTime("OUT");
+                getCurrentTime("Check-OUT");
                 is_PunchIn=true;
                 }
                 break;
@@ -52,9 +62,24 @@ private Button button_In_Time,button_Out_Time,button_punch;
         display(strDate,punchtime);
     }
 
-    private void display(String num,String punchtime) {
+    private void display(String num, String punchtime) {
         txtVw_val_status.setText(num);
         button_punch.setText(punchtime);
     }
 
+    public void doWork() {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                try{
+                    //TextView txtCurrentTime= (TextView)findViewById(R.id.myText);
+                    Date dt = new Date();
+                    int hours = dt.getHours();
+                    int minutes = dt.getMinutes();
+                    int seconds = dt.getSeconds();
+                    String curTime = hours + ":" + minutes + ":" + seconds;
+                    txtVw_val_status.setText(curTime);
+                }catch (Exception e) {e.printStackTrace();}
+            }
+        });
+    }
 }
